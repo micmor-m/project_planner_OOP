1170922c3885ea0b13afd0864504df16df12cb23
mic <micmor.m@gmail.com>
2020-12-02 23:12:30 -0500

    update text and event listener to the button after swithProject

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,23 +1,50 @@
+class DOMHelper {
+  //to remove the eventListener to the button before add a new one
+  static clearEventListeners(element) {
+    const clonedElement = element.cloneNode(true);
+    element.replaceWith(clonedElement);
+    return clonedElement;
+  }
+
+  static moveElement(elementId, newDestinationSelector) {
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(newDestinationSelector);
+    console.log(element);
+    console.log(destinationElement);
+    destinationElement.append(element);
+  }
+}
+
 class Tooltip {}
 
 //to convert the element lists in to an object
 class ProjectItem {
   //to be able to use the method switchProject from the ProjectList class I need to pass it as parameter
-  constructor(id, updateProjectListFunction) {
+  constructor(id, updateProjectListFunction, type) {
     this.id = id;
     this.updateProjectListHandler = updateProjectListFunction;
     this.connectSwitchButton();
-    this.connectMoreInfoButton();
+    this.connectMoreInfoButton(type);
   }
 
   connectMoreInfoButton() {}
 
-  connectSwitchButton() {
+  connectSwitchButton(type) {
     //since I have available the id of the item I can get all li element by id
     const projectItemElement = document.getElementById(this.id);
-    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    let switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn = DOMHelper.clearEventListeners(switchBtn);
+    switchBtn.textContent = type === "active" ? "Finish" : "Activate";
     //when pressing switchBtn I want to remove the il from one list and add it to the other
-    switchBtn.addEventListener("click", this.updateProjectListHandler);
+    switchBtn.addEventListener(
+      "click",
+      this.updateProjectListHandler.bind(null, this.id)
+    );
+  }
+
+  update(updateProjectListsFn, type) {
+    this.updateProjectListsHandler = updateProjectListsFn;
+    this.connectSwitchButton(type);
   }
 }
 
@@ -29,7 +56,7 @@ class ProjectList {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
       this.projects.push(
-        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+        new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
       );
     }
     console.log(this.projects);
@@ -43,6 +70,10 @@ class ProjectList {
     //1 add the project received as argumet to the array
     this.projects.push(project);
     //2 add the new project to the DOM
+    //could be add here below but for practice purpose create a new class
+    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    //after the switch I need to update the button with the new section Id and caption
+    project.update(this.switchProject.bind(this), this.type);
   }
 
   switchProject(projectId) {
@@ -59,7 +90,7 @@ class ProjectList {
 class App {
   static init() {
     const activeProjectList = new ProjectList("active");
-    const finishedProjectList = new ProjectList("fineshed");
+    const finishedProjectList = new ProjectList("finished");
     //here I want to call the addProject function but of the other instance
     //for this reason I need to bind to the function the right parameter
     activeProjectList.setSwitchHandlerFunction(

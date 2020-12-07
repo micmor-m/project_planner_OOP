63f10dfcfafbf020b3c2c238453c0fc36a915f46
mic <micmor.m@gmail.com>
2020-12-07 09:36:47 -0500

    add infinite scroll to event page

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -13,3 +13,20 @@ button.addEventListener("click", buttonClickHandler);
 setTimeout(() => {
   button.removeEventListener("click", buttonClickHandler);
 }, 2000);
+
+//infinite scrolling example
+//zom-in the browser to let the vertical scroll appear
+let curElementNumber = 0;
+
+function scrollHandler() {
+  const distanceToBottom = document.body.getBoundingClientRect().bottom;
+
+  if (distanceToBottom < document.documentElement.clientHeight + 150) {
+    const newDataElement = document.createElement("div");
+    curElementNumber++;
+    newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`;
+    document.body.append(newDataElement);
+  }
+}
+
+window.addEventListener("scroll", scrollHandler);

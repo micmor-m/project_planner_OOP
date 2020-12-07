47914aff75a0d5b8dc9f27d6fd9581c932591603
mic <micmor.m@gmail.com>
2020-12-07 09:54:18 -0500

    add prevent default to the submit button of the form

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -30,3 +30,10 @@ function scrollHandler() {
 }
 
 window.addEventListener("scroll", scrollHandler);
+
+//to prevent default action of submit form
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+});
